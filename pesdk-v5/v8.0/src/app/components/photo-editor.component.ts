f377894f0ab5a4787b2aafbb6ce3796f7b10ac46
Mathias Schaefer (molily) <molily@mailbox.org>
2020-07-10 15:20:55 +0200

    Expose initEditor and editor for E2E tests

--- pesdk-v5/v8.0/src/app/components/photo-editor.component.ts
+++ pesdk-v5/v8.0/src/app/components/photo-editor.component.ts
@@ -1,4 +1,6 @@
 import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core'
+// @ts-ignore
+import { deepmergeAll, Configuration } from 'photoeditorsdk/no-polyfills'
 import { EditorApi, PhotoEditorSDKUI } from 'photoeditorsdk/no-polyfills'
 
 const license = "";
@@ -17,19 +19,26 @@ export class PhotoEditorComponent implements AfterViewInit {
   public editor: EditorApi;
 
   ngAfterViewInit() {
-    this.instantiateEditor();
+    this.initEditor();
+    // Make initEditor public for the Cypress test
+    (window as any).initEditor = this.initEditor.bind(this)
   }
 
-  async instantiateEditor() {
-    try {
-      this.editor = await PhotoEditorSDKUI.init({
-        license,
-        container: this.container.nativeElement,
-        image: this.src,
-        assetBaseUrl: "/assets/photoeditorsdk",
-      });
-    } catch (error) {
-      console.log(error);
+  async initEditor(config: Partial<Configuration> = {}) {
+    if (this.editor) {
+      this.editor.dispose()
     }
+    const defaultConfig: Configuration = {
+      license,
+      container: this.container.nativeElement,
+      image: this.src,
+      assetBaseUrl: "/assets/photoeditorsdk",
+    }
+    const editor = await PhotoEditorSDKUI.init(
+      deepmergeAll([defaultConfig, config])
+    )
+    this.editor = editor
+    // @ts-ignore this is for the cypress test
+    window.editor = editor
   }
 }
