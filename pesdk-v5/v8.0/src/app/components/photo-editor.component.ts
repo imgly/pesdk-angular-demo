8aced39c800da353b7f6c90e0861cca16dfcbfa9
Mathias Schaefer (molily) <molily@mailbox.org>
2020-07-16 14:50:18 +0200

    Rename E2E API

--- pesdk-v5/v8.0/src/app/components/photo-editor.component.ts
+++ pesdk-v5/v8.0/src/app/components/photo-editor.component.ts
@@ -20,25 +20,25 @@ export class PhotoEditorComponent implements AfterViewInit {
 
   ngAfterViewInit() {
     this.initEditor();
-    // Make initEditor public for the Cypress test
-    (window as any).initEditor = this.initEditor.bind(this)
+    // Make the value global for the Cypress E2E test
+    (window as any).initPesdk = this.initEditor.bind(this);
   }
 
   async initEditor(config: Partial<Configuration> = {}) {
     if (this.editor) {
-      this.editor.dispose()
+      this.editor.dispose();
     }
     const defaultConfig: Configuration = {
       license,
       container: this.container.nativeElement,
       image: this.src,
       assetBaseUrl: "/assets/photoeditorsdk",
-    }
+    };
     const editor = await PhotoEditorSDKUI.init(
       deepmergeAll([defaultConfig, config])
-    )
-    this.editor = editor
-    // @ts-ignore this is for the cypress test
-    window.editor = editor
+    );
+    this.editor = editor;
+    // Make the value global for the Cypress E2E test
+    (window as any).pesdkEditor = editor;
   }
 }
